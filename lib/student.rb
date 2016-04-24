c87e7d49580155f843834a333d5cafcdee41e052
halfields <hfields697@aol.com>
2016-04-24 17:22:46 +0000

    trying to get program to create 'students' table

--- lib/student.rb
+++ lib/student.rb
@@ -1,2 +1,17 @@
 class Student
+
+  attr_accessor :id, :name, :tagline, :github, :twitter, :blog_url, :image_url, :biography
+
+  def Student.create_table
+    "CREATE TABLE students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      tagline TEXT,
+      github TEXT,
+      twitter TEXT,
+      blog_url TEXT,
+      image_url TEXT,
+      biography TEXT
+      )"
+  end
 end
